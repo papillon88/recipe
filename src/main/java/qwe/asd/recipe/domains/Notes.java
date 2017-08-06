73bbe8ea0720f351847ec1169aac432ff0be4473
papillon <devopreet.ch@gmail.com>
2017-08-06 12:15:52 +0530

    added entities recipe and notes

---
+++ src/main/java/qwe/asd/recipe/domains/Notes.java
@@ -0,0 +1,49 @@
+package qwe.asd.recipe.domains;
+
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String recipeNotes;
+
+    @OneToOne
+    private Recipe recipe;
+
+    /*******************
+     ******************
+     G and S
+     ******************
+     *******************/
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
