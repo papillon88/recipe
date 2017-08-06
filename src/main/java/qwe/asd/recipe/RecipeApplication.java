079c153e3a28997be67a811a3172ac67a2df7bcb
papillon <devopreet.ch@gmail.com>
2017-08-06 10:11:58 +0530

    Initial commit

---
+++ src/main/java/qwe/asd/recipe/RecipeApplication.java
@@ -0,0 +1,12 @@
+package qwe.asd.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeApplication.class, args);
+	}
+}
