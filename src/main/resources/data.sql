b23730811d13bf0ef0c722fe71d3efe9fd2b0b5a
papillon <devopreet.ch@gmail.com>
2017-08-07 12:58:20 +0530

    added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into category (description) values ('american');
+insert into category (description) values ('indian');
+insert into category (description) values ('italian');
+insert into category (description) values ('mexican');
+insert into unit_of_measure (uom) values ('teaspoon');
+insert into unit_of_measure (uom) values ('tablespoon');
+insert into unit_of_measure (uom) values ('cup');
+insert into unit_of_measure (uom) values ('pinch');
+insert into unit_of_measure (uom) values ('ounce');
