bc421675be0b14a753c59655853eaa0104583863
papillon <devopreet.ch@gmail.com>
2017-08-20 11:16:31 +0530

    added category and categoryCommand converters

---
+++ src/main/java/qwe/asd/recipe/converters/CategoryCommandToCategory.java
@@ -0,0 +1,25 @@
+package qwe.asd.recipe.converters;
+
+import com.sun.istack.internal.Nullable;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import qwe.asd.recipe.commands.CategoryCommand;
+import qwe.asd.recipe.domains.Category;
+
+@Component
+public class CategoryCommandToCategory implements Converter<CategoryCommand,Category>{
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Category convert(CategoryCommand source) {
+        if(source == null)
+            return null;
+
+        final Category category = new Category();
+        category.setId(source.getId());
+        category.setDescription(source.getDescription());
+        return category;
+    }
+}
---
+++ src/main/java/qwe/asd/recipe/converters/CategoryToCategoryCommand.java
@@ -0,0 +1,25 @@
+package qwe.asd.recipe.converters;
+
+import com.sun.istack.internal.Nullable;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import qwe.asd.recipe.commands.CategoryCommand;
+import qwe.asd.recipe.domains.Category;
+
+@Component
+public class CategoryToCategoryCommand implements Converter<Category,CategoryCommand> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public CategoryCommand convert(Category source) {
+        if(source == null)
+            return  null;
+
+        final CategoryCommand categoryCommand = new CategoryCommand();
+        categoryCommand.setId(source.getId());
+        categoryCommand.setDescription(source.getDescription());
+        return categoryCommand;
+    }
+}
