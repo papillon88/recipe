701d0f5c5b40948d2b714350b1c6352addd1c1b1
papillon <devopreet.ch@gmail.com>
2017-09-01 17:31:56 -0500

    added mapping to create new recipe

--- src/main/java/qwe/asd/recipe/controllers/RecipeController.java
+++ src/main/java/qwe/asd/recipe/controllers/RecipeController.java
@@ -4,9 +4,12 @@ package qwe.asd.recipe.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import qwe.asd.recipe.commands.RecipeCommand;
+import qwe.asd.recipe.domains.Recipe;
 import qwe.asd.recipe.services.RecipeService;
 
 @Controller
@@ -27,6 +30,20 @@ public class RecipeController {
         return "show";
     }
 
+
+    @RequestMapping(value = {"/recipe/new"}, method = RequestMethod.GET)
+    public String createRecipe(Model model){
+        model.addAttribute("recipe",new RecipeCommand());
+        return "create";
+    }
+
+
+    @RequestMapping(value = {"/recipe"},method = RequestMethod.POST)
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand){
+        RecipeCommand recipeCommand1 = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:/";
+    }
+
     public RecipeService getRecipeService() {
         return recipeService;
     }
@@ -34,4 +51,6 @@ public class RecipeController {
     public void setRecipeService(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
+
+
 }
