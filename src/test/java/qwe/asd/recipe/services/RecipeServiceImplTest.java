c43237d6af873b9d11716de48ead8cce61e700fc
papillon <devopreet.ch@gmail.com>
2017-09-02 14:04:00 -0500

    edited service IT

--- src/test/java/qwe/asd/recipe/services/RecipeServiceImplTest.java
+++ src/test/java/qwe/asd/recipe/services/RecipeServiceImplTest.java
@@ -6,6 +6,8 @@ import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import qwe.asd.recipe.commands.RecipeCommand;
+import qwe.asd.recipe.converters.*;
 import qwe.asd.recipe.domains.Recipe;
 import qwe.asd.recipe.repositories.RecipeRepo;
 
@@ -19,32 +21,51 @@ public class RecipeServiceImplTest {
 
     @Mock
     private RecipeRepo recipeRepo;
+    private RecipeToRecipeCommand recipeToRecipeCommand;
+    private IngredientToIngredientCommand ingredientToIngredientCommand;
+    private CategoryToCategoryCommand categoryToCategoryCommand;
+    private NoteToNoteCommand noteToNoteCommand;
+    private UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand;
 
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         recipeService = new RecipeServiceImpl(recipeRepo);
+        recipeToRecipeCommand = new RecipeToRecipeCommand();
+
+        ingredientToIngredientCommand = new IngredientToIngredientCommand();
+        unitOfMeasureToUnitOfMeasureCommand = new UnitOfMeasureToUnitOfMeasureCommand();
+        ingredientToIngredientCommand.setUnitOfMeasureToUnitOfMeasureCommand(unitOfMeasureToUnitOfMeasureCommand);
+
+        categoryToCategoryCommand = new CategoryToCategoryCommand();
+        noteToNoteCommand = new NoteToNoteCommand();
+
+        recipeToRecipeCommand.setNoteToNoteCommand(noteToNoteCommand);
+        recipeToRecipeCommand.setIngredientToIngredientCommand(ingredientToIngredientCommand);
+        recipeToRecipeCommand.setCategoryToCategoryCommand(categoryToCategoryCommand);
+        recipeService.setRecipeToRecipeCommand(recipeToRecipeCommand);
     }
 
     @Test
-    public void getAllRecipes() throws Exception {
+    public void testGetAllRecipeCommands() throws Exception {
         Set<Recipe> recipeSet = new HashSet<>();
         recipeSet.add(new Recipe());
         Mockito.when(recipeRepo.findAll()).thenReturn(recipeSet);
-        Set<Recipe> recipes = recipeService.getAllRecipes();
+
+        Set<RecipeCommand> recipes = recipeService.getAllRecipeCommands();
 
         assertEquals(recipes.size(),1);
         Mockito.verify(recipeRepo,Mockito.times(1)).findAll();
     }
 
     @Test
-    public void getRecipeById() throws Exception {
+    public void testGetRecipeCommandById() throws Exception {
         Recipe recipe = new Recipe();
         recipe.setId(1L);
         Optional<Recipe> recipeOptional = Optional.of(recipe);
         Mockito.when(recipeRepo.findById(Matchers.anyLong())).thenReturn(recipeOptional);
-        Recipe recipeReceived = recipeService.getRecipeById(1L);
+        RecipeCommand recipeReceived = recipeService.getRecipeCommandById(1L);
         assertNotNull(recipeReceived);
         Mockito.verify(recipeRepo,Mockito.never()).findAll();
         Mockito.verify(recipeRepo,Mockito.times(1)).findById(Matchers.anyLong());
